d49c79aa1ce5ffb86dd4e3a767d61e67ffaa5160
Hoàng Trần Mạnh Dũng <dungd7dz@gmail.com>
2026-06-22 16:31:33 +0700

    bo thu muc MVC

--- HoangTranManhDungMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HoangTranManhDungMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
